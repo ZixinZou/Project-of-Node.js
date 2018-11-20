bd75b039788f01d71e0f92be6f5f35ec8d88b2a0
SIPILE Freder <freder.sipile@efrei.net>
2018-11-20 00:51:04 +0000

    Replace index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });*/
-var list_m = require('../models/list');
+let list_m = require('../node_modules/list');
 
 router.get('/', function(req, res, next) {
-    list_m.getIndexList(function(result){
+    list_m.getIndexList(function(result) {
         res.render('index', { data:result }); // Sélectionnez le modèle d'index et transmettez les données
     })
 });
