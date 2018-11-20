13f8ecb4fec3903b47053ec0628c5ebe35d871fe
SIPILE Freder <freder.sipile@efrei.net>
2018-11-20 00:31:43 +0000

    Replace app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var app = require('express')(),
+let app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
-    fs = require('fs'),
-    filePond = require('filepond');
+    fs = require('fs');
 
 //new version START
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session')
+let express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session');
 
-var index = require('./routes/index');
-var user = require('./routes/user');
-var list = require('./routes/list');
+let index = require('./routes/index');
+    user = require('./routes/user');
+    list = require('./routes/list');
+
 
 
 // view engine setup
@@ -73,12 +73,12 @@ io.sockets.on('connection', function (socket, pseudo) {
     });
 });
 
-server.listen(3030, () => {
-    console.log('Application démarrée sur le port "http://127.0.0.1:3030" !')
+server.listen(3031, () => {
+    console.log('Application démarrée sur le port 3031 !')
 })
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
