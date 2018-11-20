8173004bb3ddcc8a014eeceb42b08f2b3d6b6f2b
SIPILE Freder <freder.sipile@efrei.net>
2018-11-20 00:50:48 +0000

    Replace list.js

--- routes/list.js
+++ routes/list.js
@@ -1,29 +1,29 @@
-var express = require('express');
-var router = express.Router();
-var async = require('async');
+let express = require('express');
+let router = express.Router();
+let async = require('async');
 
-var list_m = require('../models/list');
+let list_m = require('../node_modules/list');
 
 
-// http://127.0.0.1:3000/list/1.html
+// http://127.0.0.1:3030/list/1.html
 router.get('/:titleid.html', function(req, res) {
 	// var titleid = req.query.titleid || 1;
 	// console.log(req.query);
 	// console.log(req.params);
-	var titleid = req.params.titleid || 1;
+	let titleid = req.params.titleid || 1;
 
 	async.parallel([
-		function(callback){
-			list_m.getListById(titleid, function(result){
-				callback(null, result[0]);
+		function (async) {
+			list_m.getListById(titleid, function(result) {
+				async(null, result[0]);
 			})
 		},
-		function(callback){
+		/*function(callback){
 			list_m.getReplyById(titleid, function(result){
 				callback(null, result);
 			})
-		},
-	], function(err, results){
+		},*/
+	], function (err, results) {
 		// console.log( results );
 		// res.json(results);
 		res.render('list', { data:results });
@@ -31,12 +31,12 @@ router.get('/:titleid.html', function(req, res) {
 	
 });
 
-router.get('/addreply', function(req, res){
+/*router.get('/addreply', function(req, res){
 	if(req.session.user){
 		var titleid = parseInt(req.query.titleid),
 			content = req.query.content,
 			userid = req.session.user.userid,
-			createtime = parseInt(Date.now()/1000);
+			createtime = Date.now();
 
 		var params = {titleid:titleid, userid:userid, content:content, createtime:createtime};
 		console.log(params);
@@ -44,6 +44,7 @@ router.get('/addreply', function(req, res){
 			// console.log(result);
 			if(result.affectedRows){
 				res.json({code:0, msg:'Répondre avec succès', data:{rid:result.insertId ,createtime:createtime}});
+
 			}else{
 				res.json({code:2, msg:'La réponse a échoué, veuillez réessayer'})
 			}
@@ -52,27 +53,27 @@ router.get('/addreply', function(req, res){
 	}else{
 		res.json({code:1, msg:'Vous n\'êtes pas connecté'})
 	}
-});
+});*/
 
-router.get('/addtopic', function(req, res){
-	if(req.session.user){
-		var title = req.query.title,
+router.get('/addtopic', function(req, res) {
+	if (req.session.user) {
+		let title = req.query.title,
 			content = req.query.content,
             userid = req.session.user.userid,
-			createtime = parseInt(Date.now()/1000);
+			createtime = new Date();
 
-		var params = {userid:userid, title:title, content:content, createtime:createtime};
+		let params = {userid:userid, title:title, content:content, createtime:createtime};
 
-		list_m.addTopic(params, function(result){
+		list_m.addTopic(params, function(result) {
 			// console.log(result);
-			if(result.affectedRows){
+			if (result.affectedRows) {
 				res.json({code:0, msg:'Ajouté avec succès', data:{url:'/list/'+result.insertId+'.html', title:title, author:req.session.user.username, createtime:createtime}});
-			}else{
+			} else {
 				res.json({code:2, msg:'Ajouter échoué, veuillez réessayer'})
 			}
 		});
 		
-	}else{
+	} else {
 		res.json({code:1, msg:'Vous n\'êtes pas connecté'})
 	}
 })
